fd2c3af524d9fecbe1dd8f158e6821ff659726bc
mihaiLambru <mihailambru99@gmail.com>
2021-03-11 18:06:47 +0200

    Am adaugat metoda in IReader

--- Seminar2/src/ro/ase/cts/clase/readers/ElevReader.java
+++ Seminar2/src/ro/ase/cts/clase/readers/ElevReader.java
@@ -20,18 +20,14 @@ public class ElevReader extends IReader {
         List<Aplicant> elevi = new ArrayList<Aplicant>();
 
         while (input2.hasNext()) {
-            String nume = input2.next();
-            String prenume = input2.next();
-            int varsta = input2.nextInt();
-            int punctaj = input2.nextInt();
-            int nr = input2.nextInt();
-            String[] vect = new String[5];
-            for (int i = 0; i < nr; i++)
-                vect[i] = input2.next();
+            Elev elev = new Elev();
+            super.citireAplicant(input2,elev);
             int clasa = input2.nextInt();
             String tutore = input2.next();
-            Elev e = new Elev(nume, prenume, varsta, punctaj, nr, vect, clasa, tutore);
-            elevi.add(e);
+            elev.setClasa(clasa);
+            elev.setTutore(tutore);
+
+            elevi.add(elev);
         }
 
         input2.close();
--- Seminar2/src/ro/ase/cts/clase/readers/IReader.java
+++ Seminar2/src/ro/ase/cts/clase/readers/IReader.java
@@ -4,13 +4,33 @@ import ro.ase.cts.clase.Aplicant;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public abstract class IReader {
     protected String numeFisier;
+
+
 
     public IReader(String numeFisier) {
         this.numeFisier = numeFisier;
     }
 
     public abstract List<Aplicant> readAplicanti() throws FileNotFoundException, NumberFormatException;
+
+    public void citireAplicant(Scanner input, Aplicant aplicant){
+        String nume = input.next();
+        String prenume = input.next();
+        int varsta = input.nextInt();
+        int punctaj = input.nextInt();
+        int nr = input.nextInt();
+        String[] vect = new String[nr];
+        for (int i = 0; i < nr; i++)
+            vect[i] = input.next();
+
+        aplicant.setNume(nume);
+        aplicant.setPrenume(prenume);
+        aplicant.setVarsta(varsta);
+        aplicant.setPunctaj(punctaj);
+        aplicant.setVectorDenumiri(vect,nr);
+    }
 }
