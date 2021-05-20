38bf92c1134384eb355e64ad988bdc3f397bc3f7
mihaiLambru <mihailambru99@gmail.com>
2021-05-20 18:58:00 +0300

    Teste student

--- Teste/src/tests/StudentTest.java
+++ Teste/src/tests/StudentTest.java
@@ -11,7 +11,7 @@ public class StudentTest {
         String nume = "Costica";
         Student student = new Student(nume);
 
-        assertEquals(nume,student.getNume());
+        assertEquals("Costica",student.getNume());
         assertNotNull(student.getNote());
     }
 
@@ -19,14 +19,22 @@ public class StudentTest {
     public void testConstructorFaraPanametrii(){
         Student student = new Student();
 
-        assertNotNull(student.getNote());
-        assertNotNull(student.getNume());
+        assertNotNull("Atributul note nu a fost initializat",student.getNote());
+        assertNotNull("Atributul nume nu a fost initializat",student.getNume());
     }
 
     @Test
     public void testAdaugaNota(){
         Student student = new Student();
         int nota = 5;
+        student.adaugaNota(nota);
+
+        assertEquals(nota,student.getNota(0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testAdaugaNotaNegativa(){
+        Student student = new Student();
+        int nota = -5;
         student.adaugaNota(nota);
 
         assertEquals(nota,student.getNota(0));
@@ -85,5 +93,42 @@ public class StudentTest {
         student.adaugaNota(7);
 
         assertFalse(student.areRestante());
+    }
+
+    @Test
+    public void testGetNotaExceptie(){
+        Student student = new Student();
+        student.adaugaNota(5);
+        student.adaugaNota(7);
+//1
+        try{
+            //2
+            student.getNota(5);
+            //3
+            fail("nu trebuia sa ajunga aici. Metoda nu arunca exceptie");
+        } catch (IndexOutOfBoundsException e) {
+            //4
+        }
+        catch (Exception e){
+            fail("Metoda arunca o alta exceptie.");
+        }
+        //5
+
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNotaExceptieV4(){
+        Student student = new Student();
+        student.adaugaNota(5);
+        student.adaugaNota(7);
+
+        student.getNota(5);
+    }
+    @Test
+    public void testGetNotaExceptieV5(){
+        Student student = new Student();
+        student.adaugaNota(5);
+        student.adaugaNota(7);
+
+        //assertThrows(IndexOutOfBoundsException.class, ()-> {student.getNota(5);});
     }
 }
